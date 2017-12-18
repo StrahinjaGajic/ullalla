416059b858b490ed82922eacd1b76e5d6c7926b9
orokusaki91 <31542903+orokusaki91@users.noreply.github.com>
2017-12-18 13:01:27 +0100

    fix home controller undefined offset

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,29 +14,36 @@ class HomeController extends Controller
 		$user = Auth::user();
 		$defaultPackageExpired = null;
 		if ($user) {
-			$expiryDatePackage = getPackageExpiryDate(getDaysForExpiry($user->package1_id)[0]);
-			$defaultPackageExpired = DB::table('users')
-			->leftJoin('notifications', 'users.id', '=', 'notifications.notifiable_id')
-			->where('users.id', $user->id)
-			->where('notifications.title', 'Default Package Expiration')
-			->whereBetween('users.package1_expiry_date', [Carbon::now(), $expiryDatePackage])->first();
+			if ($user->package1_id) {
+				$expiryDatePackage = getPackageExpiryDate(getDaysForExpiry($user->package1_id)[0]);
+				$defaultPackageExpired = DB::table('users')
+				->leftJoin('notifications', 'users.id', '=', 'notifications.notifiable_id')
+				->where('users.id', $user->id)
+				->where('notifications.title', 'Default Package Expiration')
+				->whereBetween('users.package1_expiry_date', [Carbon::now(), $expiryDatePackage])->first();
+			}
 
-			$expiryDatePackage = getPackageExpiryDate(getDaysForExpiry($user->package2_id)[0]);
-			$gotmPackageExpired = DB::table('users')
-			->leftJoin('notifications', 'users.id', '=', 'notifications.notifiable_id')
-			->where('users.id', $user->id)
-			->where('notifications.title', 'Girl of The Month Package Expiration')
-			->whereBetween('users.package2_expiry_date', [Carbon::now(), $expiryDatePackage])->first();
+
+			if ($user->package2_id) {
+				$expiryDatePackage = getPackageExpiryDate(getDaysForExpiry($user->package2_id)[0]);
+				$gotmPackageExpired = DB::table('users')
+				->leftJoin('notifications', 'users.id', '=', 'notifications.notifiable_id')
+				->where('users.id', $user->id)
+				->where('notifications.title', 'Girl of The Month Package Expiration')
+				->whereBetween('users.package2_expiry_date', [Carbon::now(), $expiryDatePackage])->first();
+			}
 		}
 
 		$user = Auth::guard('local')->user();
 		if ($user) {
-			$expiryDatePackage = getPackageExpiryDate(getDaysForExpiryLocal($user->package1_duration)[0]);
-			$localDefaultPackageExpired = DB::table('users')
-			->leftJoin('notifications', 'users.id', '=', 'notifications.notifiable_id')
-			->where('users.id', $user->id)
-			->where('notifications.title', 'Local Default Package Expiration')
-			->whereBetween('users.package1_expiry_date', [Carbon::now(), $expiryDatePackage])->first();
+			if ($user->package1_duration) {
+				$expiryDatePackage = getPackageExpiryDate(getDaysForExpiryLocal($user->package1_duration)[0]);
+				$localDefaultPackageExpired = DB::table('users')
+				->leftJoin('notifications', 'users.id', '=', 'notifications.notifiable_id')
+				->where('users.id', $user->id)
+				->where('notifications.title', 'Local Default Package Expiration')
+				->whereBetween('users.package1_expiry_date', [Carbon::now(), $expiryDatePackage])->first();
+			}
 		}
 
 		return view('pages.home', compact('defaultPackageExpired', 'gotmPackageExpired', 'localDefaultPackageExpired'));
