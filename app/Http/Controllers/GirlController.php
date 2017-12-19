2b5085dc31c3b5c34ed64e1f94edd3f9ede2ae9d
orokusaki91 <31542903+orokusaki91@users.noreply.github.com>
2017-12-19 20:33:34 +0100

    12

--- app/Http/Controllers/GirlController.php
+++ app/Http/Controllers/GirlController.php
@@ -21,6 +21,9 @@ class GirlController extends Controller
 
 		$users = DB::table('users')->leftJoin('prices', 'users.id', '=', 'prices.user_id');
 
+
+		dd(Session::get('users'));
+
 		if (Session::has('users')) {
 			$userss = Session::get('users');
 		}
