c1a3d671f4fd9805bca43c2052db8214477466ff
orokusaki91 <31542903+orokusaki91@users.noreply.github.com>
2017-12-19 14:00:21 +0100

    fix again packages start date

--- resources/views/pages/profile/packages.blade.php
+++ resources/views/pages/profile/packages.blade.php
@@ -166,13 +166,13 @@
 	var end = new Date();
 	end.setFullYear(end.getFullYear() + 1);
 
-	var package2ExpiryDate = '{{ $user->package2_expiry_date }}';
+	var $package2ExpiryDate = '{{ $user->package2_expiry_date }}';
 
 	var defaultPackageStartDate = JSON.parse('{!! json_encode([$user->package1_expiry_date]) !!}');
 	var defaultPackageStartDate = new Date(defaultPackageStartDate[0].date);
 	var defaultPackageStartDate = new Date() > defaultPackageStartDate ? new Date() : defaultPackageStartDate;
 	
-	var gotmPackageStartDate = package2ExpiryDate != '' ? JSON.parse('{!! json_encode([$package2ExpiryDate]) !!}') : start;
+	var gotmPackageStartDate = package2ExpiryDate != '' ? JSON.parse('{!! json_encode([$user->package2_expiry_date]) !!}') : start;
 	var gotmPackageStartDate = new Date(gotmPackageStartDate[0].date);
 	var gotmPackageStartDate = new Date() > gotmPackageStartDate ? new Date() : gotmPackageStartDate;
 
