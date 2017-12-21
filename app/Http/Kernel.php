4f58b1c42d03e0b29eebf87226217f0d8271ba62
orokusaki91 <31542903+orokusaki91@users.noreply.github.com>
2017-12-21 01:32:40 +0100

    fix kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -28,6 +28,7 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
+            \App\Http\Middleware\DownForMaintenance::class,
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
@@ -36,7 +37,6 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\LanguageChooser::class,
-            \App\Http\Middleware\DownForMaintenance::class,
         ],
 
         'api' => [
