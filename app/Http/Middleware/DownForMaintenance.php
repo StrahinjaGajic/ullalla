7c5f832f29eed76e021065e8bb252fc51368d6c5
orokusaki91 <31542903+orokusaki91@users.noreply.github.com>
2017-12-22 14:47:19 +0100

    sd

--- app/Http/Middleware/DownForMaintenance.php
+++ app/Http/Middleware/DownForMaintenance.php
@@ -25,6 +25,7 @@ class DownForMaintenance
             '109.122.99.8',
             '24.135.165.252',
             '212.41.97.214',
+            '77.243.25.10',
         ];
 
         if (app()->environment() == 'production' && !in_array($_SERVER['REMOTE_ADDR'], $ipAddresses)) {
