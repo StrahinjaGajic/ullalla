daf2995f15cddc5e3aee6ac92a0f0cf2eca694e8
orokusaki91 <31542903+orokusaki91@users.noreply.github.com>
2017-12-22 13:24:12 +0100

    sd

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -11,66 +11,64 @@
     <div class="container">
         <div class="col-md-6 col-md-offset-3">
             <div class="card">
-               <img src="img/logo.png" width="150" height="150" alt="">
-                <h3 class="card-header text-center">Ullalla</h3>
-                <div class="card-block">
-                    {!! Form::open(['url' => 'signup']) !!}
-                    <div class="form-group row {{ $errors->has('username') ? 'has-error' : '' }}">
-                        <label for="username" class="col-3 col-form-label"></label>
-                        <div class="col-9">
-                            <input id="username" class="form-control {{ $errors->has('username') ? 'form-control-error' : '' }}" type="text" name="username" placeholder="{{ __('labels.username') }}" value="{{ old('username') }}" autofocus>
-                            @if ($errors->has('username'))
-                            <div class="help-block">{{ $errors->first('username') }}</div>
-                            @endif
-                        </div>
+             <div class="card-block">
+                {!! Form::open(['url' => 'signup']) !!}
+                <div class="form-group row {{ $errors->has('user_type') ? ' has-error' : '' }}">
+                    <label for="user_type" class="col-3 col-form-label">{{ __('fields.type') }}</label>
+                    <div class="col-9">
+                        <select name="user_type" id="user_type" class="form-control {{ $errors->has('user_type') ? ' form-control-error' : '' }}">
+                            @foreach ($userTypes as $userType)
+                            <option value="{{ $userType->id }}">{{ $userType->user_type_name }}</option>
+                            @endforeach
+                        </select>
+                        @if ($errors->has('user_type'))
+                        <div class="help-block">{{ $errors->first('user_type') }}</div>
+                        @endif
                     </div>
-                    <div class="form-group row {{ $errors->has('email') ? 'has-error' : '' }}">
-                        <label for="email" class="col-3 col-form-label"></label>
-                        <div class="col-9">
-                            <input id="email" class="form-control {{ $errors->has('email') ? 'form-control-error' : '' }}" type="text" name="email" placeholder="{{ __('labels.email') }}" value="{{ old('email') }}">
-                            @if ($errors->has('email'))
-                            <div class="help-block">{{ $errors->first('email') }}</div>
-                            @endif
-                        </div>
-                    </div>
-                    <div class="form-group row {{ $errors->has('password') ? ' has-error' : '' }}">
-                        <label for="password" class="col-3 col-form-label"></label>
-                        <div class="col-9">
-                            <input id="password" class="form-control{{ $errors->has('password') ? ' form-control-error' : '' }}" type="password" placeholder="{{ __('labels.password') }}" name="password">
-                            @if ($errors->has('password'))
-                            <div class="help-block">{{ $errors->first('password') }}</div>
-                            @endif
-                        </div>
-                    </div>
-                    <div class="form-group row {{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                        <label for="password_confirmation" class="col-3 col-form-label"></label>
-                        <div class="col-9">
-                            <input id="password_confirmation" class="form-control {{ $errors->has('password_confirmation') ? ' form-control-error' : '' }}" type="password" placeholder="{{ __('labels.confirm_password') }}" name="password_confirmation">
-                            @if ($errors->has('password_confirmation'))
-                            <div class="help-block">{{ $errors->first('password_confirmation') }}</div>
-                            @endif
-                        </div>
-                    </div>
-                    <div class="form-group row {{ $errors->has('user_type') ? ' has-error' : '' }}">
-                        <label for="user_type" class="col-3 col-form-label">{{ __('fields.type') }}</label>
-                        <div class="col-9">
-                            <select name="user_type" id="user_type" class="form-control {{ $errors->has('user_type') ? ' form-control-error' : '' }}">
-                                @foreach ($userTypes as $userType)
-                                <option value="{{ $userType->id }}">{{ $userType->user_type_name }}</option>
-                                @endforeach
-                            </select>
-                            @if ($errors->has('user_type'))
-                            <div class="help-block">{{ $errors->first('user_type') }}</div>
-                            @endif
-                        </div>
-                    </div>
-                    <div class="text-center">
-                        <button type="submit" class="btn btn-default">Register{{ __('buttons.register') }}</button>
-                    </div>
-                    {!! Form::close() !!}
                 </div>
+                <div class="form-group row {{ $errors->has('username') ? 'has-error' : '' }}">
+                    <label for="username" class="col-3 col-form-label"></label>
+                    <div class="col-9">
+                        <input class="form-control {{ $errors->has('username') ? 'form-control-error' : '' }}" type="text" name="username" placeholder="{{ __('labels.username') }}" value="{{ old('username') }}" autofocus>
+                        @if ($errors->has('username'))
+                        <div class="help-block">{{ $errors->first('username') }}</div>
+                        @endif
+                    </div>
+                </div>
+                <div class="form-group row {{ $errors->has('email') ? 'has-error' : '' }}">
+                    <label for="email" class="col-3 col-form-label"></label>
+                    <div class="col-9">
+                        <input id="email" class="form-control {{ $errors->has('email') ? 'form-control-error' : '' }}" type="text" name="email" placeholder="{{ __('labels.email') }}" value="{{ old('email') }}">
+                        @if ($errors->has('email'))
+                        <div class="help-block">{{ $errors->first('email') }}</div>
+                        @endif
+                    </div>
+                </div>
+                <div class="form-group row {{ $errors->has('password') ? ' has-error' : '' }}">
+                    <label for="password" class="col-3 col-form-label"></label>
+                    <div class="col-9">
+                        <input id="password" class="form-control{{ $errors->has('password') ? ' form-control-error' : '' }}" type="password" placeholder="{{ __('labels.password') }}" name="password">
+                        @if ($errors->has('password'))
+                        <div class="help-block">{{ $errors->first('password') }}</div>
+                        @endif
+                    </div>
+                </div>
+                <div class="form-group row {{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
+                    <label for="password_confirmation" class="col-3 col-form-label"></label>
+                    <div class="col-9">
+                        <input id="password_confirmation" class="form-control {{ $errors->has('password_confirmation') ? ' form-control-error' : '' }}" type="password" placeholder="{{ __('labels.confirm_password') }}" name="password_confirmation">
+                        @if ($errors->has('password_confirmation'))
+                        <div class="help-block">{{ $errors->first('password_confirmation') }}</div>
+                        @endif
+                    </div>
+                </div>
+                <div class="text-center">
+                    <button type="submit" class="btn btn-default">{{ __('buttons.register') }}</button>
+                </div>
+                {!! Form::close() !!}
             </div>
         </div>
     </div>
+</div>
 </div>
 @stop
