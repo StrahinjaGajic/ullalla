0041903a52a18f518e2579a32331f150f22aacc9
Balsa <mirkovicbalsa.rs@gmail.com>
2017-12-25 13:09:17 +0100

    Balsa New

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,39 +1,31 @@
-@extends('layouts.app')
+@extends('layouts.app') @section('title', '| Forgot Password') @section('styles')
+<link rel="stylesheet" href="{{ asset('css/components/reset.css') }}"> @stop @section('content')
+<div class="wrapper">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h1>Forgot password?</h1>
+            <small>Enter E-mail to help us identify you.</small>
+        </div>
+        <div class="panel-body">
 
-@section('title', '| Forgot Password')
-
-@section('styles')
-<link rel="stylesheet" href="{{ asset('css/components/reset.css') }}">
-@stop
-
-@section('content')
-    <div class="row">
-        <div class="col-md-6 col-md-offset-3">
-            <div class="panel panel-default">
-                <div class="panel-heading">{{ __('buttons.rrl') }}</div>
-                <div class="panel-body">
-
-                @if (session('status'))
-                    <div class="alert alert-success">
-                        {{ session('status') }}
-                    </div>
-                @endif
-
-                    {!! Form::open(['url' => 'password/email']) !!}
-                        <div class="form-group {{ $errors->has('email') ? 'has-error' : '' }}">
-                            {{ Form::email('email', null, ['class' => 'form-control', 'placeholder' => __('labels.email')]) }}
-                            @if ($errors->has('email'))
-                                <strong><span class="help-block">
-                                    {{ $errors->first('email') }}
-                                </span></strong>
-                            @endif
-                        </div>
-                        <div class="form-group">
-                            {{ Form::submit(__('buttons.rrl'), ['class' => 'btn btn-primary']) }}
-                        </div>
-                    {!! Form::close() !!}
-                </div>
+            @if (session('status'))
+            <div class="alert alert-success">
+                {{ session('status') }}
             </div>
+            @endif {!! Form::open(['url' => 'password/email']) !!}
+            <div class="    form-group {{ $errors->has('email') ? 'has-error' : '' }}">
+
+                {{ Form::email('email', null, ['class' => 'form-control', 'placeholder' => __('labels.email')]) }} @if ($errors->has('email'))
+                <strong><span class="help-block">
+                                    {{ $errors->first('email') }}
+                                </span></strong> @endif
+
+            </div>
+            <div class="form-group">
+                {{ Form::submit(__('buttons.rrl'), ['class' => 'btn btn-primary']) }}
+            </div>
+            {!! Form::close() !!}
         </div>
     </div>
+</div>
 @stop
