67242141d083fc8fa2dfb854544778a55b401980
orokusaki91 <31542903+orokusaki91@users.noreply.github.com>
2018-01-10 21:20:25 +0100

    add services seeder

--- database/seeds/ServiceTableSeeder.php
+++ database/seeds/ServiceTableSeeder.php
@@ -14,158 +14,464 @@ class ServiceTableSeeder extends Seeder
     {
         $service = new Service;
         $service->service_name_de = 'Position 69';
+        $service->service_name_en = 'Position 69';
+        $service->service_name_fr = 'Position 69';
+        $service->service_name_it = 'Posizione 69';
         $service->save();
 
         $service = new Service;
         $service->service_name_de = 'Anal Sex';
+        $service->service_name_en = 'Anal Sex';
+        $service->service_name_fr = 'Sexe anal';
+        $service->service_name_it = 'Sesso anale';
         $service->save();
 
         $service = new Service;
         $service->service_name_de = 'In den Mund spritzen';
+        $service->service_name_en = 'Cum in Mouth';
+        $service->service_name_fr = 'Ejaculation dans la bouche';
+        $service->service_name_it = 'Sborrata in Bocca';
         $service->save();
 
         $service = new Service;
         $service->service_name_de = 'Ins Gesicht spritzen';
+        $service->service_name_en = 'Cum on Face';
+        $service->service_name_fr = 'Ejaculation faciale';
+        $service->service_name_it = 'Sborrata in faccia';
         $service->save();
 
         $service = new Service;
-        $service->service_name_de = 'Sextoys';
+        $service->service_name_de = 'Dildo Spiele/Toys';
+        $service->service_name_en = 'Dildo Play/Toys';
+        $service->service_name_fr = 'Jeux avec gode/sextoys';
+        $service->service_name_it = 'Dildo/Toys';
         $service->save();
 
         $service = new Service;
         $service->service_name_de = 'Küssen';
+        $service->service_name_en = 'Kissing';
+        $service->service_name_fr = 'Bisous';
+        $service->service_name_it = 'Bacio';
         $service->save();
 
         $service = new Service;
         $service->service_name_de = 'Französisch mit';
+        $service->service_name_en = 'Blowjob with Condom';
+        $service->service_name_fr = 'Fellation avec préservatif';
+        $service->service_name_it = 'Pompino con Preservativo';
         $service->save();
 
         $service = new Service;
         $service->service_name_de = 'Französisch ohne';
+        $service->service_name_en = 'Blowjob without Condom';
+        $service->service_name_fr = 'Fellation sans préservatif';
+        $service->service_name_it = 'Pompino senza preservativo';
+        $service->save();
+
+        $service = new Service;
+        $service->service_name_de = 'Franz. ohne bis zum Abschluss';
+        $service->service_name_en = 'Blowjob without Condom to Completion';
+        $service->service_name_fr = 'Fellation sans préservatif jusqu\'à l\'éjaculation';
+        $service->service_name_it = 'Pompino senza preservativo completo';
         $service->save();
 
         $service = new Service;
         $service->service_name_de = 'Girlfriend Sex';
+        $service->service_name_en = 'Girlfriend Sex';
+        $service->service_name_fr = 'Comme avec une petite amie (GFE)';
+        $service->service_name_it = 'Esperienza con Fidanzata (GFE)';
         $service->save();
 
         $service = new Service;
-        $service->service_name_de = 'Küssen';
+        $service->service_name_de = 'Zungenküsse';
+        $service->service_name_en = 'French Kissing';
+        $service->service_name_fr = 'Embrasser avec la langue';
+        $service->service_name_it = 'Bacio alla francese';
         $service->save();
 
         $service = new Service;
         $service->service_name_de = 'Sex in allen Positionen';
+        $service->service_name_en = 'Sex in Different Positions';
+        $service->service_name_fr = 'Sexe dans différentes positions';
+        $service->service_name_it = 'Sesso in posizioni diverse';
         $service->save();
 
         $service = new Service;
         $service->service_name_de = 'Körperbesamung';
+        $service->service_name_en = 'Cumshot on body (COB)';
+        $service->service_name_fr = 'Ejaculation sur le corps';
+        $service->service_name_it = 'Sborrata sul corpo (COB)';
         $service->save();
 
         $service = new Service;
+        $service->service_name_de = 'Feinmassage';
+        $service->service_name_en = 'Intimate massage';
+        $service->service_name_fr = 'Massage intime';
+        $service->service_name_it = 'Massaggio parti intime';
+        $service->save();
+
+        $service = new Service;
+        $service->service_name_de = 'Küssen bei Sympathie';
+        $service->service_name_en = 'Kissing if good chemistry';
+        $service->service_name_fr = 'Bisous selon l\'alchimie';
+        $service->service_name_it = 'Bacio in caso di buona empatia';
+        $service->save();
+    
+        $service = new Service;
         $service->service_name_de = 'Handjob';
+        $service->service_name_en = 'Handjob';
+        $service->service_name_fr = 'Handjob';
+        $service->service_name_it = 'Sega';
         $service->save();
 
         $service = new Service;
         $service->service_name_de = 'Spanisch';
+        $service->service_name_en = 'Titjob';
+        $service->service_name_fr = 'Branlette espagnole';
+        $service->service_name_it = 'Spagnola';
         $service->save();
 
         $service = new Service;
-        $service->service_name_de = 'Ganzkörpermassage';
-        $service->save();
-
-        $service = new Service;
-        $service->service_name_de = 'Erotische Massage';
+        $service->service_name_de = 'Erotische Massagen';
+        $service->service_name_en = 'Erotic massage';
+        $service->service_name_fr = 'Massage érotique';
+        $service->service_name_it = 'Massaggio erotico';
         $service->save();
 
         $service = new Service;
         $service->service_name_de = 'Tantra';
-        $service->save();
-
-        $service = new Service;
-        $service->service_name_de = 'Prostatamassage';
+        $service->service_name_en = 'Tantric';
+        $service->service_name_fr = 'Tantrique';
+        $service->service_name_it = 'Tantrico';
         $service->save();
 
         $service = new Service;
         $service->service_name_de = 'Deep Throat';
+        $service->service_name_en = 'Deep Throat';
+        $service->service_name_fr = 'Gorge profonde';
+        $service->service_name_it = 'Deep Throat';
         $service->save();
 
         $service = new Service;
         $service->service_name_de = 'Gangbang';
+        $service->service_name_en = 'Gangbang';
+        $service->service_name_fr = 'Gang-bang';
+        $service->service_name_it = 'Sesso di Gruppo';
+        $service->save();
+
+        $service = new Service;
+        $service->service_name_de = 'Double Penetration (DP)';
+        $service->service_name_en = 'Double Penetration (DP)';
+        $service->service_name_en = 'Double pénétration';
+        $service->service_name_it = 'Doppia penetrazione (DP)';
+        $service->save();
+
+        $service = new Service;
+        $service->service_name_de = 'Eierlecken';
+        $service->service_name_en = 'Ball Licking and Sucking';
+        $service->service_name_fr = 'Lécher et sucer les testicules';
+        $service->service_name_it = 'Leccare e succhiare le palle';
+        $service->save();
+
+        $service = new Service;
+        $service->service_name_de = 'Dirtytalk';
+        $service->service_name_en = 'Dirtytalk';
+        $service->service_name_fr = 'Discussions cochonnes';
+        $service->service_name_it = 'Linguaggio volgare';
+        $service->save();
+
+        $service = new Service;
+        $service->service_name_de = 'Bodyschaum';
+        $service->service_name_en = 'Foam massage';
+        $service->service_name_fr = 'Massage avec mousse';
+        $service->service_name_it = 'Massaggio con Schiuma';
+        $service->save();   
+
+        $service = new Service;
+        $service->service_name_de = 'Striptease/Lapdance';
+        $service->service_name_en = 'Striptease/Lapdance';
+        $service->service_name_fr = 'Striptease/Lapdance';
+        $service->service_name_it = 'Striptease/Lapdance';
         $service->save();
 
         $service = new Service;
         $service->service_name_de = 'Kamasutra';
+        $service->service_name_en = 'Kamasutra';
+        $service->service_name_fr = 'Kamasutra';
+        $service->service_name_it = 'Kamasutra';
+        $service->save();   
+
+        $service = new Service;
+        $service->service_name_de = 'Snowballing';
+        $service->service_name_en = 'Snowballing';
+        $service->service_name_fr = 'Snowballing';
+        $service->service_name_it = 'Snowballing';
         $service->save();
 
         $service = new Service;
-        $service->service_name_de = 'Lesbospiele';
+        $service->service_name_de = 'Lesben Sexspiele';
+        $service->service_name_en = 'Lesbian Sex Games';
+        $service->service_name_fr = 'Jeux sexuels lesbiens';
+        $service->service_name_it = 'Giochi sessuali Lesbiche';
+        $service->save();
+
+        $service = new Service;
+        $service->service_name_de = 'Mehrfachspritzer willkommen';
+        $service->service_name_en = 'Extraball';
+        $service->service_name_fr = 'Plusieurs éjaculations';
+        $service->service_name_it = 'Extraball';
+        $service->save();
+
+        $service = new Service;
+        $service->service_name_de = 'Facesitting';
+        $service->service_name_en = 'Facesitting';
+        $service->service_name_fr = 'S\'asseoir sur le visage';
+        $service->service_name_it = 'Seduta in faccia';
+        $service->save();
+
+        $service = new Service;
+        $service->service_name_de = 'Fisting';
+        $service->service_name_en = 'Firsting';
+        $service->service_name_fr = 'Fist-fucking';
+        $service->service_name_it = 'Fisting';
+        $service->save();
+
+        $service = new Service;
+        $service->service_name_de = 'Ganzkörpermassage';
+        $service->service_name_en = 'Full Body Sensual Massage';
+        $service->service_name_fr = 'Massage sensuel intégral';
+        $service->service_name_it = 'Massaggio sensuale su tutto il corpo';
+        $service->save();   
+
+        $service = new Service;
+        $service->service_name_de = 'Private Video';
+        $service->service_name_en = 'Private Video';
+        $service->service_name_fr = 'Vidéo privée';
+        $service->service_name_it = 'Video Private';
+        $service->save();
+
+        $service = new Service;
+        $service->service_name_de = 'Privates Fotoshooting';
+        $service->service_name_en = 'Private Photo';
+        $service->service_name_fr = 'Photos Privées';
+        $service->service_name_it = 'Foto Private';
         $service->save();
 
         $service = new Service;
         $service->service_name_de = 'Masturbieren';
+        $service->service_name_en = 'Masturbate';
+        $service->service_name_fr = 'Masturbation';
+        $service->service_name_it = 'Masturbazione';
+        $service->save();   
+
+        $service = new Service;
+        $service->service_name_de = 'Reizwäsche';
+        $service->service_name_en = 'Lingerie';
+        $service->service_name_fr = 'Lingerie';
+        $service->service_name_it = 'Lingerie';
         $service->save();
 
         $service = new Service;
-        $service->service_name_de = 'Pornostar service';
+        $service->service_name_de = 'Duschservice';
+        $service->service_name_en = 'Shower service';
+        $service->service_name_fr = 'Service de douche';
+        $service->service_name_it = 'Servizio Doccia';
+        $service->save();
+
+        $service = new Service;
+        $service->service_name_de = 'Prostata Massage';
+        $service->service_name_en = 'Prostate Massage';
+        $service->service_name_fr = 'Massage de la prostate';
+        $service->service_name_it = 'Massaggio alla prostata';
+        $service->save();
+
+        $service = new Service;
+        $service->service_name_de = 'Whirlpool';
+        $service->service_name_en = 'Whirlpool';
+        $service->service_name_fr = 'Bain à bulles';
+        $service->service_name_it = 'Idromassaggio';
+        $service->save();
+
+        $service = new Service;
+        $service->service_name_de = 'Pornostar Service';
+        $service->service_name_en = 'Pornstar Service';
+        $service->service_name_fr = 'Expérience de star du porno (PSE)';
+        $service->service_name_it = 'Esperienza Pornstar (PSE)';
+        $service->save();
+
+        $service = new Service;
+        $service->service_name_de = 'DUO';
+        $service->service_name_en = 'DUO';
+        $service->service_name_it = 'DUO';
+        $service->service_name_it = 'DUO';
+        $service->save();
+
+        $service = new Service;
+        $service->service_name_de = 'Intimrasur';
+        $service->service_name_en = 'Intimate shaving';
+        $service->service_name_fr = 'Epilation intime';
+        $service->service_name_it = 'Depilazione parti intime';
+        $service->save();
+
+        $service = new Service;
+        $service->service_name_de = 'Outdoor Sex';
+        $service->service_name_en = 'Outdoor Sex';
+        $service->service_name_fr = 'Sexe à l\'extérieur';
+        $service->service_name_it = 'Sesso all\'aperto';
         $service->save();
 
         $service = new Service;
         $service->service_name_de = 'Zungenanal (aktiv)';
+        $service->service_name_en = 'Rimming (give)';
+        $service->service_name_fr = 'Anulingus (actif)';
+        $service->service_name_en = 'Rimming (attivo)';
         $service->save();
 
         $service = new Service;
         $service->service_name_de = 'Zungenanal (passiv)';
+        $service->service_name_en = 'Rimming (receive)';
+        $service->service_name_fr = 'Anulingus (passif)';
+        $service->service_name_it = 'Rimming (passivo)';
         $service->save();
 
         $service = new Service;
         $service->service_name_de = 'Analmassage (aktiv)';
+        $service->service_name_en = 'Anal massage (give)';
+        $service->service_name_fr = 'Massage anal (actif)';
+        $service->service_name_it = 'Massaggio anale (attivo)';
         $service->save();
 
         $service = new Service;
         $service->service_name_de = 'Analmassage (passiv)';
+        $service->service_name_en = 'Anal massage (receive)';
+        $service->service_name_fr = 'Massage anal (passif)';
+        $service->service_name_it = 'Massaggio anale (passivo)';
         $service->save();
 
         $service = new Service;
         $service->service_name_de = 'BDSM';
+        $service->service_name_en = 'BDSM';
+        $service->service_name_fr = 'BDSM';
+        $service->service_name_it = 'BDSM';
         $service->save();
 
         $service = new Service;
         $service->service_name_de = 'Bondage';
+        $service->service_name_en = 'Bondage';
+        $service->service_name_fr = 'Bondage';
+        $service->service_name_it = 'Bondage';
         $service->save();
 
         $service = new Service;
         $service->service_name_de = 'Latex / Leder';
+        $service->service_name_en = 'Latex / Leather';
+        $service->service_name_fr = 'Cuir / latex / PVC';
+        $service->service_name_it = 'Pelle / Latex/PVC';
         $service->save();
 
         $service = new Service;
         $service->service_name_de = 'Kliniksex';
+        $service->service_name_en = 'Clinic Sex';
+        $service->service_name_fr = 'Sexe médical';
+        $service->service_name_it = 'Sesso Clinico';
         $service->save();
 
         $service = new Service;
         $service->service_name_de = 'Fetish';
+        $service->service_name_en = 'Fetish';
+        $service->service_name_fr = 'Fétischisme';
+        $service->service_name_it = 'Fetish';
         $service->save();
 
         $service = new Service;
         $service->service_name_de = 'Rollenspiele';
+        $service->service_name_en = 'Role Play and Fantasy';
+        $service->service_name_fr = 'Jeux de rôles et fantaisie';
+        $service->service_name_it = 'Gioco di Ruolo e Fantasy';
         $service->save();
 
         $service = new Service;
         $service->service_name_de = 'Natursekt (aktiv)';
+        $service->service_name_en = 'Golden Shower (give)';
+        $service->service_name_fr = 'Douche dorée (donneur)';
+        $service->service_name_it = 'Pioggia dorata (attivo)';
         $service->save();
 
         $service = new Service;
         $service->service_name_de = 'Natursekt (passiv)';
+        $service->service_name_en = 'Golden Shower (receive)';
+        $service->service_name_fr = 'Douche dorée (receveur)';
+        $service->service_name_it = 'Pioggia dorata (passivo)';
         $service->save();
 
         $service = new Service;
         $service->service_name_de = 'Domina';
+        $service->service_name_en = 'Mistress';
+        $service->service_name_fr = 'Maitresse';
+        $service->service_name_it = 'Mistress';
         $service->save();
 
         $service = new Service;
-        $service->service_name_de = 'Spanking';
+        $service->service_name_de = 'Spanking (aktiv)';
+        $service->service_name_en = 'Spanking (receive)';
+        $service->service_name_fr = 'Fessée (actif)';
+        $service->service_name_it = 'Sculacciata (attivo)';
+        $service->save();
+
+        $service = new Service;
+        $service->service_name_de = 'Spanking (passiv)';
+        $service->service_name_en = 'Spanking (receive)';
+        $service->service_name_fr = 'Fessée (passif)';
+        $service->service_name_it = 'Sculacciata (passivo)';
         $service->save();
 
         $service = new Service;
         $service->service_name_de = 'Squirting';
+        $service->service_name_en = 'Squirting';
+        $service->service_name_fr = 'Éjaculation féminine';
+        $service->service_name_it = 'Squirting';
+        $service->save();
+
+        $service = new Service;
+        $service->service_name_de = 'Kaviar KV (passiv)';
+        $service->service_name_en = 'Scat (give)';
+        $service->service_name_fr = 'Scato (actif)';
+        $service->service_name_it = 'Scat (attivo)';
+        $service->save();
+
+        $service = new Service;
+        $service->service_name_de = 'Kaviar KV (aktiv)';
+        $service->service_name_en = 'Scat (receive)';
+        $service->service_name_fr = 'Scato (passif)';
+        $service->service_name_it = 'Scat (passivo)';
+        $service->save();
+
+        $service = new Service;
+        $service->service_name_de = 'Fussfetish';
+        $service->service_name_en = 'Foot Fetish';
+        $service->service_name_fr = 'Fétischisme des pieds';
+        $service->service_name_it = 'Fetish Piedi';
+        $service->save();  
+
+        $service = new Service;
+        $service->service_name_de = 'Strap on';
+        $service->service_name_en = 'Strap on';
+        $service->service_name_fr = 'Gode-ceinture';
+        $service->service_name_it = 'Strap on';
+        $service->save();
+
+        $service = new Service;
+        $service->service_name_de = 'Devot / Sklavin (hart)';
+        $service->service_name_en = 'Submissive/Slave (hard)';
+        $service->service_name_fr = 'Soumission/esclave (hard)';
+        $service->service_name_it = 'Sottomesso / Schiavo (hard)';
+        $service->save();
+
+        $service = new Service;
+        $service->service_name_de = 'Devot / Sklavin (soft)';
+        $service->service_name_en = 'Soumission/esclave (soft)';
+        $service->service_name_fr = 'Submissive/Slave (soft)';
+        $service->service_name_it = 'Sottomesso / Schiavo (soft)';
         $service->save();
     }
 }
