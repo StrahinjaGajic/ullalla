22bc2c1d6b40fce96926a5ac5a4855186200f0be
orokusaki91 <31542903+orokusaki91@users.noreply.github.com>
2018-02-21 21:53:30 +0100

    fix girl of the motnh home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -124,10 +124,7 @@
                             </div>
                             <div class="product-content">
                                 <a class="shop-name">{{ $user->nickname }}</a>
-                                <div class="pro-price">
-                                    <p>{{ __('global.short_info') }}</p>
-                                </div>
-                                <a href="{{ url('private/' . $user->nickname) }}"><div class="product-cart">
+                                <a href="{{ url('private/' . $user->id) }}"><div class="product-cart">
                                     <button class="button">{{ __('buttons.view_profile') }}</button>
                                 </div></a>
                             </div>
