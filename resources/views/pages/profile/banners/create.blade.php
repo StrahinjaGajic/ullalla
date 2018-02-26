1a66e8e03e648f8337bf93dc997429c11ef39319
orokusaki91 <31542903+orokusaki91@users.noreply.github.com>
2018-02-26 22:25:30 +0100

    fix banners front

--- resources/views/pages/profile/banners/create.blade.php
+++ resources/views/pages/profile/banners/create.blade.php
@@ -662,13 +662,13 @@ function validateForm(el) {
 }
 
 function fixStepIndicator(n) {
-  // This function removes the "active" class of all steps...
-  var i, x = document.getElementsByClassName("step");
-  for (i = 0; i < x.length; i++) {
-    x[i].className = x[i].className.replace(" active", "");
-  }
-  //... and adds the "active" class to the current step:
-  x[n].className += " active";
+    // remove the active class of all steps
+    var i, x = document.getElementsByClassName("step");
+    for (i = 0; i < x.length; i++) {
+        x[i].className = x[i].className.replace(" active", "");
+    }
+    // add the active class to the current step
+    x[n].className += " active";
 }
 
 function removeError(that) {
