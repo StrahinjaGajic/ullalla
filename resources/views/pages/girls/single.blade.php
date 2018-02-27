1115b6a8ccf0bd912c5828ebc56acb69c0c380e3
orokusaki91 <31542903+orokusaki91@users.noreply.github.com>
2018-02-27 19:50:53 +0100

    remove owl carousel from single

--- resources/views/pages/girls/single.blade.php
+++ resources/views/pages/girls/single.blade.php
@@ -493,32 +493,4 @@ function changeToMonth(){
 	document.getElementById('month').style.display = "block";
 }
 </script>
-
-<script>
-    
-    $(document).ready(function(){
-  $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        responsiveClass:true,
-        responsive:{
-        0:{
-            items:1,
-            nav:true,
-        },
-        600:{
-            items:1,
-            nav:false,
-        },
-        1000:{
-            items:1,
-            nav:true,
-            loop:false,
-        }
-    }
-  });
-});
-    
-    </script>
-
 @stop
