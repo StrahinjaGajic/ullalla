7b432247c9ea60a770f65613205352727ef369c4
orokusaki91 <31542903+orokusaki91@users.noreply.github.com>
2018-03-01 21:09:50 +0100

    fix logo water mark

--- app/Http/Controllers/LocalController.php
+++ app/Http/Controllers/LocalController.php
@@ -122,8 +122,7 @@ class LocalController extends Controller
             $user->lng = $lng;
             $user->about_me = request('about_me');
             $user->local_type_id = request('local_type_id');
-            $user->photo = storeAndGetUploadCareFiles(request('logo'));
-            $user->photo = $user->photo. '-/overlay/1c53f72b-9fba-4dcc-94fd-29a7dce59f8f/120x25/150,140/50p/';
+            $user->photo = request('logo') ? storeAndGetUploadCareFiles(request('logo')) . '-/overlay/f90362fd-8c5f-4daf-8c1b-7f1ca3ca90f8/120x25/150,140/50p/' : NULL;
             $user->photos = $uploadedPhotos ? $inputPhotos : null;
             $user->videos = storeAndGetUploadCareFiles(request('video'));
             $user->working_time = $workingTime;
@@ -230,8 +229,7 @@ class LocalController extends Controller
             'photos' => 'numeric|min:4|max:9',
         ]);
 
-        $local->photo = storeAndGetUploadCareFiles(request('photo'));
-        $local->photo = $local->photo. '-/overlay/1c53f72b-9fba-4dcc-94fd-29a7dce59f8f/120x25/150,140/50p/';
+        $local->photo = request('photo') ? storeAndGetUploadCareFiles(request('photo')) . '-/overlay/f90362fd-8c5f-4daf-8c1b-7f1ca3ca90f8/120x25/150,140/50p/' : NULL;
         $local->photos = $uploadedPhotos ? $inputPhotos : null;
         $local->videos = storeAndGetUploadCareFiles(request('video'));
         $local->save();
