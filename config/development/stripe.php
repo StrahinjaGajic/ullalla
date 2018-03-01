deb2058771a25e322688c3bb60acee8c3fd220a5
orokusaki91 <31542903+orokusaki91@users.noreply.github.com>
2018-03-01 01:38:09 +0100

    fix stripe keys

---
+++ config/development/stripe.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+	'publishable_key' => 'pk_test_OwlA6Ol8iRgfpTRb8JZxdOR4',
+	'secret_key' => 'sk_test_AeK9s7EcbwqwKDKF0XapDIXe',
+];
--- config/stripe.php
+++ config/stripe.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-	'secret_key' => 'sk_test_AeK9s7EcbwqwKDKF0XapDIXe',
-	'publishable_key' => 'pk_test_OwlA6Ol8iRgfpTRb8JZxdOR4',
+	'publishable_key' => 'pk_live_hKZNHCr9WhLIrPqyUKg7bPP6',
+	'secret_key' => 'sk_live_mpL8LhxyFK06ZUG88o2ZxUIh',
 ];
